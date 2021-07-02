36bf5904a156805bbf42fa84f036a36e45242998
Tyrone Serdan <tyroneb.serdan@gmail.com>
2021-07-02 19:33:47 +0800

    some comments on jquery functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-$("p").css("color","red");
+// Pretty much self explanatory,
+// adds classes and removes classes
+$("p").addClass("big-title ugly-color");
+$("p").removeClass("ugly-color");
+
+
+// The equivalent of
+// innerHTML & innerText
+$("p").text("sexy wexy");
+$("p").html("<em>bruh momento</em>");
+
+// Attributes can be changed with
+// .attr
+$("a").attr("href", "http://google.com");
