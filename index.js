84f1d8cc0925e4efb45e16cc8d34531b3be6ddf4
Tyrone Serdan <tyroneb.serdan@gmail.com>
2021-07-03 23:12:04 +0800

    added more jquery notes also button on html

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+function onClick() {
+    // This is just a simple function to avoid
+    // making many anonymous functions
+    // Disregard me :D
+    alert("I've been clicked!");
+}
+
+
+
+
                                         // Okay so think of $ as document.QuerySelectorAll
                                         // but instead of being spaghetti it is very pog now
+                                        // how you essentially write stuff in jquery is
+                                        // $(selector).FUNCTION(text/attr/function)
 
 
                                         // Pretty much self explanatory,
@@ -19,9 +31,20 @@ var pTagText = $("p").text();
                                         // Attributes can be interacted with
                                         // .attr
                                         // While .removeAttr does what its told lol
+
 $("a").removeAttr("href");
 $("a").attr("href", "http://google.com");
 var aTagAttrHref = $("a").attr("href"); // console.log("aTagAttrHref");
                                         // quick sidenote, CSS classes ARE attributes :D
 var pTagClasses = $("p").attr("class"); // console.log("pTagClasses");
 
+                                        
+                                        // the .click method
+                                        // is essentially adding an
+                                        // event listener
+$("p").click(onClick)
+                                        // Want to add a ton of listeners?
+                                        // dont add a loop, just write the same thing.
+                                        // it will automatically add the listener
+                                        // to all applicable selectors :D
+$("button").click(onClick)
