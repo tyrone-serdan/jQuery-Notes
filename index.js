3767d10141930fbf14cb3090fbfd088ae12bad6b
Tyrone Serdan <tyroneb.serdan@gmail.com>
2021-07-04 23:37:37 +0800

    before, after, prepend, append definitions added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function onClick() {
+function AlertFunc() {
     // This is just a simple function to avoid
     // making many anonymous functions
     // Disregard me :D
-    alert("I've been clicked!");
+    alert("I've been called!");
 }
 
 
@@ -42,9 +42,24 @@ var pTagClasses = $("p").attr("class"); // console.log("pTagClasses");
                                         // the .click method
                                         // is essentially adding an
                                         // event listener
-$("p").click(onClick)
+$("p").click(AlertFunc);
                                         // Want to add a ton of listeners?
                                         // dont add a loop, just write the same thing.
                                         // it will automatically add the listener
                                         // to all applicable selectors :D
-$("button").click(onClick)
+$("button").click(AlertFunc);
+                                        // want to check keyboard events?
+                                        // bro there is a built in function for that
+                                        // so its pretty pog
+$("input").keyup(AlertFunc);
+
+                                        // So you wanna be extra special and make html
+                                        // changes in javascript.
+                                        // thats where these functions come in!
+$("p").before("<button> im before the p tag! </button>");   // shows before the p tag
+$("p").after("<button> im after the p tag! </button>");     // shows after the p tag
+
+$("p").prepend("<button>prepend</button>");                 // before the content, inside the tag
+$("p").append("<button>append</button>");                   // after the content, inside the tag
+
+
